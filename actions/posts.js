08a45cc165560f235394a1967eb924ef8c8f436d
waizin28 <waizinlinn.08@gmail.com>
2024-11-02 15:16:15 -0500

    added rerender for production

--- actions/posts.js
+++ actions/posts.js
@@ -46,6 +46,8 @@ export async function createPost(prevState, formData) {
     userId: 1,
   });
 
+  revalidatePath('/', 'layout');
+
   redirect('/feed');
 }
 
--- app/page.js
+++ app/page.js
@@ -13,10 +13,10 @@ export default async function Home() {
     <>
       <h1>Welcome back!</h1>
       <p>Here's what you might've missed.</p>
-      <section id="latest-posts">
-      <Suspense fallback={<p>Loading recent posts...</p>}>
-        <LatestPosts />
-      </Suspense>
+      <section id='latest-posts'>
+        <Suspense fallback={<p>Loading recent posts...</p>}>
+          <LatestPosts />
+        </Suspense>
       </section>
     </>
   );
